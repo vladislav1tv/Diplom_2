bad39fea847f925d48e56df0a76de0ed57ab524b
Владислав <qa.engineer.tv@gmail.com>
2024-11-26 19:53:51 +0300

    API-тестирование: Создание пользователя Вход пользователя Изменение данных пользователя Создание заказа Получение заказов конкретного пользователя

---
+++ src/main/java/utils/Util.java
@@ -0,0 +1,9 @@
+package utils;
+
+import org.apache.commons.lang3.RandomStringUtils;
+
+public class Util {
+    public static String randomString(int length) {
+        return RandomStringUtils.randomAlphabetic(length);
+    }
+}
